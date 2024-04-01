cb3c5f61170d63489aa6f90bd1bff0a79df98e01
Ashwin Totagi <156072456+ashhwin99@users.noreply.github.com>
2024-04-01 23:26:38 +0530

    Update MyPortfolio.jsx

--- src/Pages/Home/MyPortfolio.jsx
+++ src/Pages/Home/MyPortfolio.jsx
@@ -9,7 +9,7 @@ export default function MyPortfolio() {
           <h2 className="section--heading">My Portfolio</h2>
         </div>
         <div>
-          <button className="btn btn-github">
+          <a className="btn btn-github" href="https://github.com/ashhwin99">
             <svg
               xmlns="http://www.w3.org/2000/svg "
               width="32"
@@ -25,7 +25,7 @@ export default function MyPortfolio() {
               />
             </svg>
             Visit My GitHub
-          </button>
+          </a>
         </div>
       </div>
       <div className="portfolio--section--container">
